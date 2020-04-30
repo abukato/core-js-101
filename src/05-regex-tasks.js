c4c92a07541b970f152fd974ac7deece453b0595
abukato <artur.bukato@gmail.com>
2020-04-30 08:50:00 +0300

    feat: regex tasks resolved

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -32,9 +32,8 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-  throw new Error('Not implemented');
+  return /\{[a-f\d]{8}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{12}\}/i;
 }
-
 
 /**
  * Returns the regexp that matches all the strings from first column
@@ -54,7 +53,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  return /^[^pP\s]|pi/;
 }
 
 
@@ -78,10 +77,9 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  return new RegExp(`^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{${minLength},}$`);
 }
-
 
 module.exports = {
   getRegexForGuid,
